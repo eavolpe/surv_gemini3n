25f862baf1a95a6135174de4192a0da8fe60544a
Emilio Volpe <64241377+eavolpe@users.noreply.github.com>
2025-08-03 17:05:00 -0500

    read and from gcs

--- realtime_inference_job/gcs_functions.py
+++ realtime_inference_job/gcs_functions.py
@@ -15,4 +15,22 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob.upload_from_filename(source_file_name)
     print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
-upload_blob(bucket_name, source_file_name, destination_blob_name)
+
+from google.cloud import storage
+
+def get_urls_from_gcs(bucket_name, blob_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    
+    # Read entire text file as a string
+    content = blob.download_as_text()
+
+    # Extract and return URLs as a list
+    urls = [line.strip() for line in content.strip().splitlines() if line.strip()]
+    return urls
+
+# Example usage
+print(get_urls_from_gcs(bucket_name,"urls.txt"))
+
+#upload_blob(bucket_name, source_file_name, destination_blob_name)
